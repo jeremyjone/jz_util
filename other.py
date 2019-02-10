f892c0267c311b60243980eca514dfbd11a26b21
jeremyjone <jeremyjone@qq.com>
2019-02-10 11:39:07 +0800

    线程模块增加关闭线程方法

--- other.py
+++ other.py
@@ -13,20 +13,5 @@ __version__ = "1.0.0"
 
 
 
-def stopThread(thread):
-    '''stop_thread(Thread)  # 强制关闭线程'''
-    def _async_raise(tid, exctype):
-        """raises the exception, performs cleanup if needed"""
-        tid = ctypes.c_long(tid)
-        if not inspect.isclass(exctype):
-            exctype = type(exctype)
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
-        if res == 0:
-            raise ValueError("invalid thread id")
-        elif res != 1:
-            # """if it returns a number greater than one, you're in trouble,
-            # and you should call it again with exc=NULL to revert the effect"""
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
-            raise SystemError("PyThreadState_SetAsyncExc failed")
-    _async_raise(thread.ident, SystemExit)
+
 
---
+++ threading/__init__.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+command information
+"""
+__author__ = "jeremyjone"
+__datetime__ = "2019/02/10 12:10"
+__all__ = ["__version__", "stopThread"]
+__version__ = "1.0.0"
+
+
+
+def stopThread(thread):
+    '''stop_thread(Thread)  # 强制关闭线程'''
+    def _async_raise(tid, exctype):
+        """raises the exception, performs cleanup if needed"""
+        tid = ctypes.c_long(tid)
+        if not inspect.isclass(exctype):
+            exctype = type(exctype)
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+        if res == 0:
+            raise ValueError("invalid thread id")
+        elif res != 1:
+            # """if it returns a number greater than one, you're in trouble,
+            # and you should call it again with exc=NULL to revert the effect"""
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
+    _async_raise(thread.ident, SystemExit)
